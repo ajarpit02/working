9d1695ed90b6a07310ba8d7c33ab827190763eef
ajarpit02 <arpit.jain@techolution.com>
2025-12-03 14:15:13 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from math_utils import add
+from string_utils import to_upper
+
+def run_demo():
+    print("Add Result:", add(10, 20))
+    print("Upper Result:", to_upper("github demo"))
+
+if __name__ == "__main__":
+    run_demo()
