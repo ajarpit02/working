605cf56ebdf52c8417ed7a99ee689c1b1cf3cafd
ajarpit02 <arpit.jain@techolution.com>
2026-01-15 18:10:10 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,20 @@
+public class FunctionsOverview {
+
+    // Java function (method)
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int result = add(5, 3);
+        System.out.println("Result: " + result);
+
+        /*
+         JavaScript function example:
+
+         function add(a, b) {
+             return a + b;
+         }
+        */
+    }
+}
